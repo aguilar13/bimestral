aae177f176c8f00512ea5b887617721d76030968
T- <T-@PC190>
2017-05-23 21:42:43 -0500

    rest

---
+++ src/main/resources/public/js/primero.js
@@ -0,0 +1,8 @@
+//El siguiente es el equivalente a la clase system con out printline
+console.log("Hola mundo");
+$("#boton1").click(new function(){
+    //console.log("Haz oprimido el boton");
+    $("#algo").html("Haz oprimido el boton");
+
+}
+);
